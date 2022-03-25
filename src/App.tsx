d0bd83d0479a4b855ae218152db298c136b9f1f7
IvanMatt8 <ivanmatt1995@gmail.com>
2022-03-25 09:48:31 -0300

    :sparkles: adicionado estrutura da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import * as C from "./App.styles";
 
 const App = () => {
-  return <C.Container>...</C.Container>;
+  return (
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Sistema Financeiro Pessoal</C.HeaderText>
+      </C.Header>
+      <C.Body>...</C.Body>
+    </C.Container>
+  );
 };
 
 export default App;
+
+
