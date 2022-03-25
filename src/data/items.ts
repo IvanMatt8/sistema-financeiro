1a61bf5e7b3f692cd860e1d7daa0770a0356947f
IvanMatt8 <ivanmatt1995@gmail.com>
2022-03-25 14:32:52 -0300

    :hammer: refactor dos dias, dos itens da lista

--- src/data/items.ts
+++ src/data/items.ts
@@ -8,19 +8,19 @@ export const items: Item[] = [
     value: 32.12,
   },
   {
-    date: new Date(2021, 9, 15),
+    date: new Date(2021, 10, 16),
     category: "food",
     title: "Burguer KIng",
     value: 28,
   },
   {
-    date: new Date(2021, 9, 16),
+    date: new Date(2021, 9, 15),
     category: "rent",
     title: "Aluguel apt",
     value: 2300,
   },
   {
-    date: new Date(2021, 10, 18),
+    date: new Date(2021, 9, 15),
     category: "salary",
     title: "Salario ACME",
     value: 4500,
