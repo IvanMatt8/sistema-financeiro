40e4332d277fabc21ecfead2c393d3f14b191ede
IvanMatt8 <ivanmatt1995@gmail.com>
2022-03-25 18:07:32 -0300

    :rotating_light: testes na aplicação

--- src/data/items.ts
+++ src/data/items.ts
@@ -8,7 +8,7 @@ export const items: Item[] = [
     value: 32.12,
   },
   {
-    date: new Date(2021, 10, 16),
+    date: new Date(2021, 9, 16),
     category: "food",
     title: "Burguer KIng",
     value: 28,
